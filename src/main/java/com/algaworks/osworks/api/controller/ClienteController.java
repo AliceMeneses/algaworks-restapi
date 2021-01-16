09c50cbe1b7c5645b927409e33f720f82cd35e07
Alice <alicesilvademeneses@gmail.com>
2021-01-16 17:53:41 -0300

    Get /clientes

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -1,31 +1,23 @@
 package com.algaworks.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Alice");
-		cliente1.setEmail("alice@gmail.com");
-		cliente1.setTelefone("11 44444-4444");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("João");
-		cliente2.setEmail("joao@gmail.com");
-		cliente2.setTelefone("11 88888-8888");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();
 	}
 }
